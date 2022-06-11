75015af987fe44d27e82b1d8acb42a237e88222f
yunkyung-Hwang <dudhkd98@naver.com>
2022-06-11 18:45:03 +0900

    [ADD]: 폰트 추가

---
+++ Daylist/Daylist/Global/Extension/UIFont+.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+.swift
+//  Daylist
+//
+//  Created by 황윤경 on 2022/06/11.
+//
+
+import UIKit
+
+extension UIFont {
+    class func KyoboHandwriting(size: CGFloat) -> UIFont {
+        return UIFont(name: "KyoboHandwriting2019", size: size)!
+    }
+}
