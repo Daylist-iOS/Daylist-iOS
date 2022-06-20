770c3592af2a3442bd0206e050890ecd8a38800d
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-21 04:11:52 +0900

    [Fix]: #21 - Main 브랜치 Rebase 중 충돌 해결

---
+++ Daylist/Daylist/Screen/Detail/ViewController/DetailVC.swift
@@ -0,0 +1,15 @@
+//
+//  DetailVC.swift
+//  Daylist
+//
+//  Created by hwangJi on 2022/06/21.
+//
+
+import UIKit
+
+class DetailVC: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
