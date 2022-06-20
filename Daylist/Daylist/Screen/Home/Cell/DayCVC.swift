16cf5c18fbe39698b758a01030ba27ba984b62ab
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-20 20:24:57 +0900

    [CHORE]: 캘린더를 구성하는 Week, Day CollectionViewCell 생성

--- Daylist/Daylist/Screen/Home/Cell/DayCVC.swift
+++ Daylist/Daylist/Screen/Home/Cell/DayCVC.swift
@@ -6,7 +6,77 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+import Kingfisher
 
-class DayCVC: UICollectionViewCell {
+class DayCVC: BaseCollectionViewCell {
     
+    private var cdImageView = UIImageView()
+    private var thumbnailImageView = UIImageView()
+    private var emotionImageView = UIImageView()
+    private(set) var dayLabel = UILabel().then {
+        $0.textColor = .black
+        $0.font = UIFont.KyoboHandwriting(size: 13.0)
+    }
+    
+    override func prepareForReuse() {
+        thumbnailImageView.image = nil
+        emotionImageView.image = nil
+    }
+    
+    override func setupViews() {
+        configureUI()
+    }
+    
+    override func layoutSubviews() {
+        thumbnailImageView.layer.cornerRadius = thumbnailImageView.layer.frame.height / 2
+        thumbnailImageView.layer.masksToBounds = true
+    }
+}
+
+// MARK: - UI
+
+extension DayCVC {
+    private func configureUI() {
+        self.addSubviews([thumbnailImageView, cdImageView, emotionImageView, dayLabel])
+        
+        thumbnailImageView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(calculateHeightbyScreenHeight(originalHeight: 38))
+        }
+        
+        cdImageView.snp.makeConstraints {
+            $0.centerX.centerY.equalTo(thumbnailImageView)
+            $0.width.height.equalTo(thumbnailImageView)
+        }
+        
+        emotionImageView.snp.makeConstraints {
+            $0.bottom.trailing.equalTo(cdImageView)
+            $0.height.equalTo(calculateHeightbyScreenHeight(originalHeight: 14))
+            $0.width.equalTo(calculateHeightbyScreenHeight(originalHeight: 14) * 15 / 14)
+        }
+        
+        dayLabel.snp.makeConstraints {
+            $0.centerX.equalTo(thumbnailImageView)
+            $0.top.equalTo(cdImageView.snp.bottom).offset(3)
+        }
+    }
+}
+
+// MARK: - Custom Methods
+
+extension DayCVC {
+    func setData(model: CalendarDataResponse, dayString: String) {
+        dayLabel.text = dayString
+        cdImageView.image = UIImage(named: model.playlistID == 0 ? "dayCell_empty" : "dayCell_fill")
+        emotionImageView.image = model.playlistID == 0 ? nil : EmotionType(rawValue: model.emotion)?.emotionImage
+        downloadThumbnailImage(url: model.thumbnailImage)
+    }
+    
+    private func downloadThumbnailImage(url: String) {
+        guard let url = URL(string: url) else { return }
+        thumbnailImageView.kf.setImage(with: url)
+    }
 }
--- Daylist/Daylist/Screen/Home/Cell/WeekCVC.swift
+++ Daylist/Daylist/Screen/Home/Cell/WeekCVC.swift
@@ -6,7 +6,38 @@
 //
 
 import UIKit
+import SnapKit
+import Then
 
-class WeekCVC: UICollectionViewCell {
+class WeekCVC: BaseCollectionViewCell {
     
+    private(set) var weekLabel = UILabel().then {
+        $0.font = UIFont.KyoboHandwriting(size: 14.0)
+    }
+
+    override func setupViews() {
+        configureUI()
+    }
 }
+
+// MARK: - UI
+
+extension WeekCVC {
+    private func configureUI() {
+        self.addSubview(weekLabel)
+        
+        weekLabel.snp.makeConstraints {
+            $0.centerX.centerY.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - Custom Methods
+
+extension WeekCVC {
+    func setData(weekString: String) {
+        weekLabel.text = weekString
+    }
+}
+
+
