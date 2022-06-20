316a1fce5597743fbe1b6802c919dd262e21237f
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-21 03:11:10 +0900

    [CHORE]: #15 - 검색 요청/응답 데이터 모델 구현

---
+++ Daylist/Daylist/Screen/Search/Model/Request/SearchDataRequest.swift
@@ -0,0 +1,13 @@
+//
+//  SearchDataRequest.swift
+//  Daylist
+//
+//  Created by hwangJi on 2022/06/21.
+//
+
+import Foundation
+
+struct SearchDataRequest {
+    var userId: Int
+    var keyword: String
+}
