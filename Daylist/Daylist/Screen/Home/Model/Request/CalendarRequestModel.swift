2c9b7f2be8248531da664b556489e188ba52ae00
hwangJi-dev <melon2228@swu.ac.kr>
2022-06-20 20:31:11 +0900

    [ADD]: #9 - 월별 캘린더 조회 API 통신을 위한 Req, Res Model 생성

---
+++ Daylist/Daylist/Screen/Home/Model/Request/CalendarRequestModel.swift
@@ -0,0 +1,14 @@
+//
+//  CalendarRequestModel.swift
+//  Daylist
+//
+//  Created by hwangJi on 2022/06/20.
+//
+
+import Foundation
+
+struct CalendarRequestModel {
+    var userId: Int
+    var year: String
+    var month: String
+}
